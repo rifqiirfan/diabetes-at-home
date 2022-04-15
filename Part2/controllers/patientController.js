7dda2c7de30a201675761d7ceb0e43cc6d1a571f
JustinGu988 <guhaoyu1019@sina.com>
2022-04-15 19:18:03 +0800

    tbd: add entry data

--- Part2/controllers/patientController.js
+++ Part2/controllers/patientController.js
@@ -1,5 +1,6 @@
 // link to model
 const allPatientData = require('../models/patientDetail')
+const patientRecords = require('../models/patientRecords')
 
 // handle request to get all data
 const getAllPatientData = (req, res) => {
@@ -10,10 +11,12 @@ const getAllPatientData = (req, res) => {
 const getPatientDataById = (req, res) => {
     // search the database by ID
     const data = allPatientData.find(data => data.id === req.params.id)
+    let patientId = 1;
+    const record = patientRecords.find((r) => r.patientID == patientId)
 
     // return data if this ID exists
     if (data) {
-        res.render('oneData.hbs', { oneItem: data })
+        res.render('oneData.hbs', { onePatient: data, record: record })
     } else {
         res.sendStatus(404)
     }
@@ -21,8 +24,42 @@ const getPatientDataById = (req, res) => {
 
 // add an object to the database
 const insertData = (req, res) => {
-    const { id, firstName, lastName } = req.body
-    allPatientData.push({ id, firstName, lastName })
+    const { bgl, weight, doit, exercise } = req.body
+    let patientId = 1;
+    const record = patientRecords.find((r) => r.patientID == patientId)
+    patientRecords.push({
+        patientID: 1,
+        dateHistory: [
+            {
+                actualDate: '2019-01-01',
+                valueReceived: {
+                    bgl: {
+                        value: 8.0,
+                        timeRecorded: "08:05",
+                    },
+
+                    weight: {
+                        value: 80,
+                        timeRecorded: "08:10",
+                    },
+                    doit: {
+                        value: 2,
+                        timeRecorded: "23:30",
+                    },
+                    exercise: {
+                        value: 13451,
+                        timeRecorded: "21:00",
+                    }
+                },
+
+                commentText: {
+                    text: "Sample text from the patient 10001",
+                    timeRecorded: '',
+                },
+            },
+        ]
+    },
+    )
     return res.redirect('back')
 }
 
