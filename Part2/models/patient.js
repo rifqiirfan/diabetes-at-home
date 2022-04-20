02b4ebee9a32d67a9f979499b4b954c17e14b899
JustinGu988 <guhaoyu1019@sina.com>
2022-04-20 15:26:12 +0800

    meeting

---
+++ Part2/models/patient.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const schema = new mongoose.Schema({
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    screenName: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    yearOfBirth: { type: Number, required: true },
+    textBio: { type: String, required: true },
+    records: {
+        actualDate: { type: String, required: true }
+    }
+
+
+})
+const Patient = mongoose.model('Patient', schema)
+module.exports = Patient
