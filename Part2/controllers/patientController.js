aec5483697163876d573d8fd84b0a20b83d4da74
JustinGu988 <guhaoyu1019@sina.com>
2022-04-25 17:43:25 +0800

    merge Jasmine's work from her branch. Entry and view page

--- Part2/controllers/patientController.js
+++ Part2/controllers/patientController.js
@@ -3,7 +3,7 @@ const allPatientData = require('../models/patient')
 const patientRecords = require('../models/record')
 
 
-const getAllPatientData = async(req, res, next) => {
+const getAllPatientData = async (req, res, next) => {
     try {
         const allPatients = await allPatientData.find().lean()
         return res.render('allData', { data: allPatients })
@@ -12,64 +12,48 @@ const getAllPatientData = async(req, res, next) => {
     }
 }
 
-// handle request to get one data instance
-const getPatientDataById = async(req, res, next) => {
+// get info for one patient
+const getPatientDataById = async (req, res, next) => {
     try {
         // get data for a specific patient from patient schema (fname, lname...)
         const data = await allPatientData.findById(req.params.patient_id).lean()
-
-        // get an array of recordID from patient schema
-        const all_rec_id = data.records
-            // initialize an empty array to store record data
-        const all_rec = []
-            // loop through the recordID array in patient schema
-            // use the recordID to retrieve record data from record schema
-            // store the record data in all_rec[]
-        for (var i = 0; i < all_rec_id.length; i++) {
-            const one_rec = await patientRecords.findById(data.records[i].recordID).lean()
-            all_rec.push(one_rec)
-        }
-
-        if (!data) {
-            return res.sendStatus(404)
-        }
-        return res.render('oneData.hbs', { onePatient: data, record: all_rec })
-
+        if (!data) return res.sendStatus(404)
+        return res.render('oneData.hbs', { onePatient: data })
     } catch (err) {
         return next(err)
     }
 }
 
 // add an object to the database
-const insertData = async(req, res, next) => {
+const insertData = async (req, res, next) => {
     try {
         // RECORD CREATION AND INSERTION:
         // capture input value
         const { bgl, weight, doit, exercise, comment } = req.body
-            // create a record
+        // create a record
         var new_rec = new patientRecords({
-                "patientId": "1001",
-                "recordDate": Date.now(),
-                "bgl_fullName": "blood glocose level",
-                "bgl_status": "recorded",
-                "bgl_value": bgl,
-                "bgl_createdAt": Date.now(),
-                "weight_fullName": "weight",
-                "weight_status": "recorded",
-                "weight_value": weight,
-                "weight_createdAt": Date.now(),
-                "doit_fullName": "doses of insulin taken",
-                "doit_status": "recorded",
-                "doit_value": doit,
-                "doit_createdAt": Date.now(),
-                "ex_fullName": "exercise",
-                "ex_status": "recorded",
-                "ex_value": exercise,
-                "ex_createdAt": Date.now(),
-                "cmt_fullName": comment,
-                "cmt_value": "I feel good"
-            })
-            // insert the new record to db
+            "patientId": "1001",
+            "recordDate": Date.now(),
+            "bgl_fullName": "blood glocose level",
+            "bgl_status": "recorded",
+            "bgl_value": bgl,
+            "bgl_createdAt": Date.now(),
+            "weight_fullName": "weight",
+            "weight_status": "recorded",
+            "weight_value": weight,
+            "weight_createdAt": Date.now(),
+            "doit_fullName": "doses of insulin taken",
+            "doit_status": "recorded",
+            "doit_value": doit,
+            "doit_createdAt": Date.now(),
+            "ex_fullName": "exercise",
+            "ex_status": "recorded",
+            "ex_value": exercise,
+            "ex_createdAt": Date.now(),
+            "cmt_fullName": "comment",
+            "cmt_value": comment
+        })
+        // insert the new record to db
         await new_rec.save()
 
         // RECORD UPDATING FOR PATIENT:
@@ -87,7 +71,9 @@ const insertData = async(req, res, next) => {
 
 }
 
-const entryPatientData = async(req, res, next) => {
+
+// entry data page
+const entryPatientData = async (req, res, next) => {
     try {
         return res.render('entry.hbs')
     } catch (err) {
@@ -95,9 +81,30 @@ const entryPatientData = async(req, res, next) => {
     }
 }
 
-const viewPatientData = async(req, res, next) => {
+
+// view history data page
+const viewPatientData = async (req, res, next) => {
     try {
-        return res.render('view.hbs')
+        // get data for a specific patient from patient schema (fname, lname...)
+        const data = await allPatientData.findById(req.params.patient_id).lean()
+
+        // get an array of recordID from patient schema
+        const all_rec_id = data.records
+        // initialize an empty array to store record data
+        const all_rec = []
+        // loop through the recordID array in patient schema
+        // use the recordID to retrieve record data from record schema
+        // store the record data in all_rec[]
+        for (var i = 0; i < all_rec_id.length; i++) {
+            const one_rec = await patientRecords.findById(data.records[i].recordID).lean()
+            all_rec.push(one_rec)
+        }
+
+        if (!data) {
+            return res.sendStatus(404)
+        }
+        return res.render('view.hbs', { onePatient: data, record: all_rec })
+
     } catch (err) {
         return next(err)
     }
@@ -110,5 +117,4 @@ module.exports = {
     insertData,
     entryPatientData,
     viewPatientData
-
 }
--- Part2/routes/patientRouter.js
+++ Part2/routes/patientRouter.js
@@ -9,20 +9,11 @@ const patientController = require('../controllers/patientController')
 // add a route to handle the GET request for all demo data
 patientRouter.get('/', patientController.getAllPatientData)
 
-// add a route to handle entry patient data 
-patientRouter.get('/entry', patientController.entryPatientData)
+patientRouter.get('/entry/:patient_id', patientController.entryPatientData)
 
-// add a route to handle entry patient data 
-patientRouter.get('/view', patientController.viewPatientData)
+patientRouter.get('/view/:patient_id', patientController.viewPatientData)
 
-// add a route to handle entry patient data 
-patientRouter.get('/view/:patient_id', patientController.getPatientDataById)
-
-// add a route to handle the GET request for one data instance
-patientRouter.get('', patientController.getPatientDataById)
-
-
-
+patientRouter.get('/:patient_id', patientController.getPatientDataById)
 
 // add a new JSON object to the database
 patientRouter.post('/', patientController.insertData)
