c1933493e6ae0440ba2d9da0cea5ef42a92d5821
JustinGu988 <guhaoyu1019@sina.com>
2022-04-27 16:46:53 +0800

    change db name

--- Part2/models/index.js
+++ Part2/models/index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'Diabetes_at_Home_Patient'
+    dbName: 'Diabetes@Home'
 })
 // Exit on error
 const db = mongoose.connection.on('error', err => {
