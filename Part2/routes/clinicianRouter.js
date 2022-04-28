8b4081f64dada091397ac725a5294aa17eb071ab
RuifengZhan <88077095+RuifengZhan@users.noreply.github.com>
2022-04-28 23:31:09 +0800

    correct the timezone format

--- Part2/routes/clinicianRouter.js
+++ Part2/routes/clinicianRouter.js
@@ -5,6 +5,6 @@ const clinicRouter = express.Router();
 
 clinicRouter.get('/', controller.getAllPatientData);
 clinicRouter.get("/:id", controller.renderRecordData);
-clinicRouter.post("/:id", controller.updateRecord);
+
 
 module.exports = clinicRouter;
