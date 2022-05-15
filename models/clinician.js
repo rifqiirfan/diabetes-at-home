7a85e4e557f3f4ccc754c622d9a3c3a9dfe2f1df
xubw <bx2@student.unimelb.edu.au>
2022-05-15 19:52:19 +0800

    create clinician schema and view/entry for clinical notes

---
+++ models/clinician.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const clincianSchema = new mongoose.Schema({
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  clinicalNotes: {
+    createdAt: { type: String, default: null },
+    createdBy: { type: String, required: true, trim: true },
+    subjectiveData: { type: String, required: true, trim: true },
+    objectiveData: { type: String, required: true, trim: true },
+    assessmentData: { type: String, required: true, trim: true },
+    plan: { type: String, required: true, trim: true },
+    additionalNotes: { type: String, trim: true },
+  },
+  patientID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: true,
+  },
+});
+const Clinician = mongoose.model("Clinician", clinicianSchema);
+module.exports = Clinician;
--- models/patient.js
+++ models/patient.js
@@ -1,73 +1,77 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const patientSchema = new mongoose.Schema({
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
-    screenName: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    yearOfBirth: { type: String, required: true },
-    textBio: { type: String, required: true },
-    supportMessage: { type: String, required: true },
-    data: {
-        bgl: {
-            availability: { type: Boolean, default: false },
-            status: {
-                type: String,
-                enum: ['recorded', 'unrecorded', 'no need'],
-                default: 'unrecorded',
-            },
-            minValue: { type: Number, default: 4 },
-            maxValue: { type: Number, default: 7 },
-            minTime: { type: Number, default: 0 },
-            maxTime: { type: Number, default: 24 },
-        },
-        weight: {
-            availability: { type: Boolean, default: false },
-            status: {
-                type: String,
-                enum: ['recorded', 'unrecorded', 'no need'],
-                default: 'unrecorded',
-            },
-            minValue: { type: Number, default: 55 },
-            maxValue: { type: Number, default: 85 },
-            minTime: { type: Number, default: 0 },
-            maxTime: { type: Number, default: 24 },
-        },
-        doit: {
-            availability: { type: Boolean, default: false },
-            status: {
-                type: String,
-                enum: ['recorded', 'unrecorded', 'no need'],
-                default: 'unrecorded',
-            },
-            minValue: { type: Number, default: 1 },
-            maxValue: { type: Number, default: 3 },
-            minTime: { type: Number, default: 0 },
-            maxTime: { type: Number, default: 24 },
-        },
-        exercise: {
-            availability: { type: Boolean, default: false },
-            status: {
-                type: String,
-                enum: ['recorded', 'unrecorded', 'no need'],
-                default: 'unrecorded',
-            },
-            minValue: { type: Number, default: 3000 },
-            maxValue: { type: Number, default: 7200 },
-            minTime: { type: Number, default: 0 },
-            maxTime: { type: Number, default: 24 },
-        },
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  screenName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  yearOfBirth: { type: String, required: true },
+  textBio: { type: String, required: true },
+  supportMessage: { type: String, required: true },
+  gender: { type: String, required: true },
+  age: { type: Number, required: true },
+  data: {
+    bgl: {
+      availability: { type: Boolean, default: false },
+      status: {
+        type: String,
+        enum: ["recorded", "unrecorded", "no need"],
+        default: "unrecorded",
+      },
+      minValue: { type: Number, default: 4 },
+      maxValue: { type: Number, default: 7 },
+      minTime: { type: Number, default: 0 },
+      maxTime: { type: Number, default: 24 },
     },
-    records: [{
-        recordID: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Record',
-            required: true,
-        },
-    }, ],
-})
+    weight: {
+      availability: { type: Boolean, default: false },
+      status: {
+        type: String,
+        enum: ["recorded", "unrecorded", "no need"],
+        default: "unrecorded",
+      },
+      minValue: { type: Number, default: 55 },
+      maxValue: { type: Number, default: 85 },
+      minTime: { type: Number, default: 0 },
+      maxTime: { type: Number, default: 24 },
+    },
+    doit: {
+      availability: { type: Boolean, default: false },
+      status: {
+        type: String,
+        enum: ["recorded", "unrecorded", "no need"],
+        default: "unrecorded",
+      },
+      minValue: { type: Number, default: 1 },
+      maxValue: { type: Number, default: 3 },
+      minTime: { type: Number, default: 0 },
+      maxTime: { type: Number, default: 24 },
+    },
+    exercise: {
+      availability: { type: Boolean, default: false },
+      status: {
+        type: String,
+        enum: ["recorded", "unrecorded", "no need"],
+        default: "unrecorded",
+      },
+      minValue: { type: Number, default: 3000 },
+      maxValue: { type: Number, default: 7200 },
+      minTime: { type: Number, default: 0 },
+      maxTime: { type: Number, default: 24 },
+    },
+  },
+  records: [
+    {
+      recordID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Record",
+        required: true,
+      },
+    },
+  ],
+});
 
 // create collection patients in mongodb
-const Patient = mongoose.model('Patient', patientSchema)
-module.exports = Patient
+const Patient = mongoose.model("Patient", patientSchema);
+module.exports = Patient;
