f26163e86681c29bebcce876f608a88eae781236
Rifqi Imaduddin Irfan <rifqiirfan@gmail.com>
2022-05-20 14:11:50 +0800

    Arrange form label and title

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,32 +1,42 @@
 const helpers = {
-    ifRecorded: function (status, options) {
+    ifRecorded: function(status, options) {
         if (status == 'recorded') {
             return options.fn(this)
         }
         return options.inverse(this)
     },
 
-    ifUnrecorded: function (status, options) {
+    ifUnrecorded: function(status, options) {
         if (status == 'unrecorded') {
             return options.fn(this)
         }
         return options.inverse(this)
     },
 
-    thresholdJudge: function (value, max, min, options) {
+    thresholdJudge: function(value, max, min, options) {
         if (value >= min && value <= max) {
             return options.fn(this)
         } else {
             return options.inverse(this)
         }
     },
-    ifMissing: function (status, options){
-        if ( status == 0){
+    ifMissing: function(status, options) {
+        if (status == 0) {
             return options.fn(this)
-        } else{
+        } else {
             return options.inverse(this)
         }
-    }
+    },
+    checkBadge: function(eRate, options) {
+        if (eRate >= 0.8) {
+            return options.fn(this);
+
+        }
+        return options.inverse(this);
+    },
 }
+
+
+
 
 module.exports.helpers = helpers
